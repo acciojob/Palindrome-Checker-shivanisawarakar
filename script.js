61979f033040d08a2e0c4381eebc3a4a189d3640
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-01-23 15:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
-
 function palindrome(str){
 
+	str = str.toLowerCase();
+	for (let i = 0;i<str.length/2;i++) {
+		if(str[i] !== str[str.length-1 -i]){
+			return false;
+		}
+	}
+	return true;
 }
+let str = 'RaceCar';
+console.log(palindrome(str))
 module.exports = palindrome
